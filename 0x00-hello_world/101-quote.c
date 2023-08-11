169be962ee1e7a8ecc9f9b52a6a88a3da1a1a755
Mahmouddobie <my503262@gmail.com>
2023-08-11 02:36:09 -0700

    my commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * return:1
+ * main - entry point
+ */
+int main(void)
+{
+     fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-1\n9", stdout);
+        return(1);
+}
