8fd965d9b3380b4de6539f5b3488b7da81c7a683
Mahmouddobie <my503262@gmail.com>
2023-08-11 03:07:46 -0700

    my commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,14 @@
-#include <stdio.h>
 #include <unistd.h>
+
 /**
- * return:1
- * main - entry point
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *                followed by a new line, to standard error.
+ *
+ * Return:1
  */
 int main(void)
 {
-     fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-1\n9", stdout);
-        return(1);
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+
+	return (1);
 }
