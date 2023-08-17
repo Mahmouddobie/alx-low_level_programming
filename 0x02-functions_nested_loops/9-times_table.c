14fa38001ec42256738e607d9dc0f33638a725c2
Mahmouddobie <my503262@gmail.com>
2023-08-16 19:54:35 -0700

    my commit

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table
+ *
+*/
+void times_table(void)
+{
+	int num, m, x;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar('0');
+		for (m = 1; m <= 9; m++)
+		{
+			_putchar(',');
+			_putchar(' ');
+			x = num * m;
+
+			if (x <= 9)
+				_putchar(' ');
+			else
+				 _putchar((x / 10) + '0');
+			_putchar((x % 10) + '0');
+		}
+
+		_putchar('\n');
+	}
+
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -15,4 +15,10 @@ void jack_bauer(void);
 int add(int, int);
 void print_to_98(int n);
 
+
+
+
+
+
+
 #endif
