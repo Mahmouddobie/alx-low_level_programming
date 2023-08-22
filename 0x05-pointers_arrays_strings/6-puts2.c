b86f4c3d50f964b25f93fecccb0c161c0eabbd2d
Mahmouddobie <my503262@gmail.com>
2023-08-22 16:43:35 -0700

    my commit

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,13 +9,11 @@
 
 void puts2(char *str)
 {
-	int i = 0;
+	int i;
 
-	while (str[i] != '\0')
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (i % 2 == 0)
 			putchar(s[i]);
-			++i;
 	}
 	putchar('\n');
 }
