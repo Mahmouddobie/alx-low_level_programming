e894102588949d87ee46f46ed5b060d59fe8b860
Mahmouddobie <my503262@gmail.com>
2023-08-22 16:28:44 -0700

    my commit

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts2 - prints every character of a string by new line
+ * @str: the string containing character.
+ *
+*/
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		putchar(s[i]);
+		i++;
+	}
+	putchar('\n');
+}
