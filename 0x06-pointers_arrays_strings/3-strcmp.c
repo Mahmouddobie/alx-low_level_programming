2c72baaa3532d243fe1382ae806da818d8d7e4fb
Mahmouddobie <my503262@gmail.com>
2023-08-25 12:37:20 -0700

    my commit

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -15,7 +15,7 @@ int _strcmp(char *s1, char *s2)
 
 	while (*s1 != '0' && *s2 != 0)
 	{
-		if (*s1 == *s2)
+		if (*s1 != *s2)
 		{
 			i = 1;
 		}
@@ -27,5 +27,5 @@ int _strcmp(char *s1, char *s2)
 	else
 		return (1);
 
-
+	return (*s1 - *s2);
 }
