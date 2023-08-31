7c3e0e15474010b3beac634c31716044fa650072
Mahmouddobie <my503262@gmail.com>
2023-08-31 08:58:14 -0700

    my commit

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _print_rev_recursion - function theat print string in reverse.
+ *
+ * @s: string to print.
+*/
+
+void _print_rev_recursion(char *s)
+{
+	if (*s > '\0')
+	{
+		_print_rev_recursion(s++);
+	}
+
+	putchar(*s);
+
+}
