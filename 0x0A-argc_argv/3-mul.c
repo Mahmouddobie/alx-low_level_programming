1c376f7f5fa9575552c7a84a01709845d7fd2bb2
Mahmouddobie <my503262@gmail.com>
2023-09-03 03:41:39 -0700

    my commit

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,14 +13,16 @@ int main(int argc, char **argv)
 {
 	int i, mul;
 
-	mul = atoi(argv[1]);
-	i = atoi(argv[2]);
-	printf("%d\n", mul * i);
-
 	if (argc < 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
+
+	else
+		mul = atoi(argv[1]);
+		i = atoi(argv[2]);
+		printf("%d\n", mul * i);
+
 	return (0);
 }
