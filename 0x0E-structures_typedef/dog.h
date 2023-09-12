c6d9916d5fe9964883350e7a651024e73fc44595
Mahmouddobie <my503262@gmail.com>
2023-09-12 09:01:44 -0700

    my commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - short desc
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of the dog
+*/
+
+struct dog
+{
+	char *nmae;
+	float age;
+	char *owner;
+};
